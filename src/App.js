191f806a305584d9feafaa9eec98d1fb1aa60954
suhailabdaz <suhail.abdaz@gmail.com>
2024-03-25 11:56:15 +0530

    create react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex justify-center my-[45vh] text-4xl font-bold ">
+     <h1>Not Rendered !</h1>
+    </div>
+  );
+}
+
+export default App;
